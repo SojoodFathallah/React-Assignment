daa74bcd5a3ab236a99867a0cd5d0fa539358845
SojoodFathallah <sojoodali380@gmail.com>
2025-03-19 03:13:34 +0200

    fix

--- reactassignment/vite.config.js
+++ reactassignment/vite.config.js
@@ -7,5 +7,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/"
+  base: process.env.VITE_BASE_PATH || "/React-Assignment"
 })
